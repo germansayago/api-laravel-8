75a5a5f78c5984fc9b0d5723ce8cfbc5a1e9d951
cmblue <german.sayago@gmail.com>
2021-02-10 10:43:07 -0300

    creamos nuestro primer recurso de api - posts

---
+++ api/app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function getExcerptAttribute()
+    {
+        return substr($this->content, 0, 120);
+    }
+    public function getPublishedAtAttribute()
+    {
+        return $this->created_at->format('d/m/Y');
+    }
+}
